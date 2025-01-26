7864f4b11fe5f39685046df246e9da29517e6810
Ioanna Boufidou <ioannab44@gmail.com>
2025-01-27 00:37:57 +0200

    add card's reveal animation

--- src/components/JokeInfo.vue
+++ src/components/JokeInfo.vue
@@ -1,11 +1,20 @@
 <template>
-  <div
-    class="card bg-white rounded-2xl shadow-lg hover:shadow-2xl transition p-6 flex flex-col justify-between h-64"
-    @click="handleClick"
-  >
-    <h2 class="text-lg font-bold text-gray-800">{{ setup }}</h2>
-    <p class="mt-2 text-gray-600">{{ type }}</p>
-    <p class="mt-4 text-gray-500 italic">Click to reveal the punchline</p>
+  <div class="card-container relative w-full h-full" @click="onRevealClick">
+    <div class="card-inner duration-500" :class="{ 'rotate-y-180': isRevealed }">
+      <!-- front side of the card -->
+      <div
+        class="bg-white rounded-2xl shadow-lg hover:shadow-2xl p-6 flex flex-col justify-between h-64"
+      >
+        <h2 class="text-lg font-bold text-gray-800">{{ setup }}</h2>
+        <p class="mt-4 text-gray-500 italic">Click to reveal the punchline</p>
+      </div>
+      <!-- back side of the card-- TODO: change the gray color maybe -->
+      <div
+        class="bg-gray-100 rounded-2xl shadow-lg hover:shadow-2xl p-6 flex items-center justify-center absolute inset-0 card-back"
+      >
+        <p class="text-gray-800">{{ punchline }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,21 +39,46 @@ export default {
       required: true,
     },
   },
+  data() {
+    return { isRevealed: false }
+  },
   methods: {
-    handleClick() {
-      console.log(`Punchline: ${this.punchline}`)
+    onRevealClick() {
+      this.isRevealed = !this.isRevealed
     },
   },
 }
 </script>
 
-<style>
-.box {
-  background-color: #5e31644d;
+<style scoped>
+.card-container {
+  perspective: 1000px; /* depth to 3D effect */
+  width: 100%;
+  height: 100%;
 }
 
-.box:hover {
-  background-color: #e0e0e0;
-  transform: scale(1.05);
+.card-inner {
+  transform-style: preserve-3d; /* 3D perspective */
+}
+
+.card-inner.rotate-y-180 {
+  transform: rotateY(180deg);
+}
+
+.card-back {
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+}
+
+@keyframes cardFlip {
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(90deg) scale(1.1);
+  }
+  100% {
+    transform: rotateY(180deg);
+  }
 }
 </style>
