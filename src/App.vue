4aa47129232a7c092f674dacd019d2c6a1423854
Ioanna Boufidou <ioannab44@gmail.com>
2025-01-28 01:00:51 +0200

    add the sortBy picklist

--- src/App.vue
+++ src/App.vue
@@ -119,6 +119,7 @@
             Programming Jokes
           </button>
         </div>
+        <div v-if="activeView === 'favorites'" class="flex items-center"></div>
         <form class="relative w-1/2 max-w-sm">
           <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg
@@ -160,9 +161,33 @@
       </div>
       <div v-else>
         <div>
-          <h2 class="text-2xl font-bold tracking-tight text-gray-900 py-4 px-4">
-            {{ HeaderJokeCards }}
-          </h2>
+          <div class="flex items-center justify-between w-full">
+            <div class="flex items-center">
+              <h2 class="text-2xl font-bold tracking-tight text-gray-900 py-4 px-4">
+                {{ HeaderJokeCards }}
+              </h2>
+            </div>
+            <div class="relative inline-block w-32">
+              <select
+                v-model="selectedSort"
+                class="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-gray-500 focus:border-gray-500 block w-full p-1 pr-6"
+              >
+                <option value="Random">None</option>
+                <option value="Rating">Rating</option>
+                <option value="Alphabetically">Alphabetically</option>
+              </select>
+              <svg
+                class="absolute right-1 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-500 pointer-events-none"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+              </svg>
+            </div>
+          </div>
           <div class="grid grid-cols-3 gap-8 justify-center pb-8">
             <JokeInfo
               v-for="joke in displayedJokes"
@@ -212,6 +237,7 @@ export default {
       activeView: 'all',
       dropdownOpen: false,
       searchJoke: '',
+      selectedSort: 'Random',
     }
   },
 
@@ -297,10 +323,17 @@ export default {
     displayedJokes() {
       let jokes =
         this.activeView === 'favorites' ? this.favoriteJokes : this.jokes[this.activeCategory] || []
+      // Search bar
       if (this.searchJoke) {
         jokes = jokes.filter((joke) =>
           joke.setup.toLowerCase().includes(this.searchJoke.toLowerCase()),
         )
+      }
+      // Sort picklist
+      if (this.selectedSort === 'Rating') {
+        // TODO: fix the rating
+      } else if (this.selectedSort === 'Alphabetical') {
+        jokes.sort((a, b) => a.setup.localeCompare(b.setup))
       }
       return jokes
     },
