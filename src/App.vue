d2bb25dd5a3aaba209cb7dee89bbf44055fd62c0
Ioanna Boufidou <ioannab44@gmail.com>
2025-01-28 02:45:47 +0200

    add filter jokes option

--- src/App.vue
+++ src/App.vue
@@ -167,7 +167,9 @@
                 {{ HeaderJokeCards }}
               </h2>
             </div>
+            <div class="flex gap-2">
             <div class="relative inline-block w-32">
+                <label class="block text-xs font-medium text-gray-500 px-1">Sort by</label>
               <select
                 v-model="selectedSort"
                 class="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-gray-500 focus:border-gray-500 block w-full p-1 pr-6"
@@ -177,7 +179,31 @@
                 <option value="Alphabetically">Alphabetically</option>
               </select>
               <svg
-                class="absolute right-1 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-500 pointer-events-none"
+                  class="absolute right-1 top-7 transform -translate-y-1/2 w-4 h-4 text-gray-500 pointer-events-none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
+              </div>
+              <div class="relative inline-block w-32">
+                <label class="block text-xs font-medium text-gray-500 mb-1 px-1">Filter by</label>
+                <select
+                  v-model="selectedFilter"
+                  class="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-gray-500 focus:border-gray-500 block w-full p-1 pr-6"
+                >
+                  <option value="0">None</option>
+                  <option value="1">1 Star</option>
+                  <option value="2">2 Star</option>
+                  <option value="3">3 Star</option>
+                  <option value="4">4 Star</option>
+                  <option value="5">5 Star</option>
+                </select>
+                <svg
+                  class="absolute right-1 top-7 transform -translate-y-1/2 w-4 h-4 text-gray-500 pointer-events-none"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -186,6 +212,7 @@
               >
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
               </svg>
+              </div>
             </div>
           </div>
           <div class="grid grid-cols-3 gap-8 justify-center pb-8">
@@ -240,6 +267,7 @@ export default {
       dropdownOpen: false,
       searchJoke: '',
       selectedSort: 'Random',
+      selectedFilter: 0,
     }
   },
 
