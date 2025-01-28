fb3fc6bf6dd330cbc2d4803f6cd74439164e88cf
Ioanna Boufidou <ioannab44@gmail.com>
2025-01-28 15:02:53 +0200

    fix the empty heart of favorites

--- src/components/JokeInfo.vue
+++ src/components/JokeInfo.vue
@@ -15,13 +15,18 @@
         >
           <svg
             v-if="!isFavorite"
+            heart
             xmlns="http://www.w3.org/2000/svg"
             class="w-5 h-5 text-gray-500"
-            fill="currentColor"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
             viewBox="0 0 24 24"
           >
             <path
-              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 21C12 21 3 13.625 3 8.5C3 5.739 5.239 3.5 8 3.5C9.657 3.5 11.173 4.405 12 5.708C12.827 4.405 14.343 3.5 16 3.5C18.761 3.5 21 5.739 21 8.5C21 13.625 12 21 12 21Z"
             />
           </svg>
           <svg
@@ -51,13 +56,18 @@
         >
           <svg
             v-if="!isFavorite"
+            heart
             xmlns="http://www.w3.org/2000/svg"
             class="w-5 h-5 text-gray-500"
-            fill="currentColor"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
             viewBox="0 0 24 24"
           >
             <path
-              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 21C12 21 3 13.625 3 8.5C3 5.739 5.239 3.5 8 3.5C9.657 3.5 11.173 4.405 12 5.708C12.827 4.405 14.343 3.5 16 3.5C18.761 3.5 21 5.739 21 8.5C21 13.625 12 21 12 21Z"
             />
           </svg>
           <svg
